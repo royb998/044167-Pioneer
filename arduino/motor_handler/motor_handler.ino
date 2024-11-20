1cc655638a1d3118b0c1acf9e3d867e6fa1c7e37
Roy Bar-On <royb997@gmail.com>
2024-11-20 15:48:51 +0200

    Added encoder read to arduino code

--- arduino/motor_handler/motor_handler.ino
+++ arduino/motor_handler/motor_handler.ino
@@ -5,6 +5,9 @@ const int LEFT_DIR = 8;
 const int RIGHT_DIR = 7;
 const int ENABLE = 6; // Normally low
 
+const int LEFT_ENCODER = 2;
+const int RIGHT_ENCODER = 3;
+
 typedef struct
 {
   uint8_t left_forward;
@@ -12,9 +15,24 @@ typedef struct
   uint8_t left_value;
   uint8_t right_value;
   uint8_t motors_enable;
-} packet_t;
+} input_packet_t;
 
-packet_t packet;
+input_packet_t packet;
+
+uint32_t left_count;
+uint32_t right_count;
+
+// Interrup handler for left motor encoder tick count.
+void left_inc()
+{
+  ++left_count;
+}
+
+// Interrup handler for right motor encoder tick count.
+void right_inc()
+{
+  ++right_count;
+}
 
 void setup() {
   // Initialize serial communication.
@@ -33,10 +51,24 @@ void setup() {
   packet.left_forward = 0;
   packet.left_value = 0;
   packet.motors_enable = 1;
+
+  // Initialize encoder counts.
+  left_count = 0;
+  right_count = 0;
+
+  // Set interrups for counting encoder ticks.
+  attachInterrupt(digitalPinToInterrupt(LEFT_ENCODER), &left_inc, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(RIGHT_ENCODER), &right_inc, CHANGE);
 }
 
 void loop()
 {
+  // Send encoder counts to controller.
+  Serial.write((char *)&left_count, 4);
+  Serial.write((char *)&right_count, 4);
+  left_count = 0;
+  right_count = 0;
+
   // Read values once a full packet is available.
   if (Serial.available() >= sizeof(packet))
   {
@@ -48,4 +80,6 @@ void loop()
     digitalWrite(RIGHT_DIR, !packet.right_forward);
     digitalWrite(ENABLE, packet.motors_enable);
   }
+
+  delay(100);
 }
